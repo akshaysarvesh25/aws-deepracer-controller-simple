1c40287c9857ff867818d4668da0367a88cacad7
gargivaidya <gargivaidya@tamu.edu>
2020-09-28 12:58:35 -0500

    Yaw correction

--- simulation_ws/src/deepracer_simulation/scripts/pid_controller.py
+++ simulation_ws/src/deepracer_simulation/scripts/pid_controller.py
@@ -7,20 +7,24 @@ from std_msgs.msg import Float32
 from std_msgs.msg import Float64
 from ackermann_msgs.msg import AckermannDriveStamped
 from gazebo_msgs.msg import ModelStates
+from deepracer_msgs.msg import Progress
 import PID_control
 
 flag_move = 0
 x_des = 10
-y_des = 0
+y_des = 10
 x_pub = rospy.Publisher('/vesc/low_level/ackermann_cmd_mux/output',AckermannDriveStamped,queue_size=1)
 throttle = 0.0
+heading = 0.0
 pos=[0,0]
+yaw=0.0
+
 def set_throttle_steer(data):
     #print(data.pose[1].orientation.y)
-    racecar_pose = data.pose[1]
-    pos[0] = racecar_pose.position.x
-    pos[1] = racecar_pose.position.y    
-
+    #racecar_pose = data.pose[1]
+    pos[0] = data.x
+    pos[1] = data.y
+    yaw = data.yaw
 
 def servo_commands():
 
@@ -28,25 +32,40 @@ def servo_commands():
     rospy.init_node('servo_commands', anonymous=True)
     
     msg = AckermannDriveStamped()
-    rospy.Subscriber("/gazebo/model_states", ModelStates,set_throttle_steer)
-    #print("========first throttle signal=======",throttle) 
+    rospy.Subscriber("/progress", Progress, set_throttle_steer)
+    #rospy.Subscriber("/gazebo/model_states", ModelStates,set_throttle_steer)
+     
     err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
+    heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.00001))
+    
+    print("read yaw2",yaw)
     while not err<0.5:
-        
-        throttle = 0.0
-        print("====x coord=====",pos[0])
-        control = PID_control.PID(0.0001,0,0.00001)
+             
+        print("====position=====",pos[0],pos[1])
+        speed_control = PID_control.PID(0.000001,0,0.000001)
         err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
-        throttle = control.Update(x_des-pos[0])
+        throttle = speed_control.Update(err)
+        print("distance:", err)
+        print("throttle:",throttle)
         
-        #head 
-        #steer = control.Update(head)
+        steer_control = PID_control.PID(0.0001,0,0.000001)
+        heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.01))
+        steer = steer_control.Update(heading-yaw)
+        print("yaw:",yaw)
+        print("heading:",heading)
+        print("steer_angle:",heading-yaw)
+        print("steer:",steer)
         
-        print("========throttle signal=======",throttle)
-        msg.drive.speed = throttle
+       
+        #print("========throttle signal=======",throttle)
+        msg.drive.speed = throttle 
         x_pub.publish(msg)
         time.sleep(1)
-        print("new loop")
+               
+        msg.drive.steering_angle = steer
+        x_pub.publish(msg)
+        time.sleep(1)
+        print("==============")
         
         
     msg.drive.speed = 0.0
