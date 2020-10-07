f1f7046a298329247a86f851b00cab559e83d4c7
gargivaidya <gargivaidya@tamu.edu>
2020-10-07 14:50:23 -0500

    Include waypoint tracking

--- simulation_ws/src/deepracer_simulation/scripts/pid_controller.py
+++ simulation_ws/src/deepracer_simulation/scripts/pid_controller.py
@@ -10,75 +10,64 @@ from gazebo_msgs.msg import ModelStates
 from deepracer_msgs.msg import Progress
 import PID_control
 import tf
+import pandas as pd 
 
 flag_move = 0
-x_des = 5  
-y_des = 5 
+
+#x_des = 1 
+#y_des = 0 
 x_pub = rospy.Publisher('/vesc/low_level/ackermann_cmd_mux/output',AckermannDriveStamped,queue_size=1)
 throttle = 0.0
 heading = 0.0
 pos=[0,0]
 yaw=0.0
-q=[0,0,0,0]
+global count
+count = 0
+
+     
 def set_position(data):
+    #global x_des
+    #global y_des
+            
     racecar_pose = data.pose[1]
     pos[0] = racecar_pose.position.x
     pos[1] = racecar_pose.position.y
-    
     quaternion = (
-    data.pose[1].orientation.x,
-    data.pose[1].orientation.y,
-    data.pose[1].orientation.z,
-    data.pose[1].orientation.w)
+            data.pose[1].orientation.x,
+            data.pose[1].orientation.y,
+            data.pose[1].orientation.z,
+            data.pose[1].orientation.w)
     euler = tf.transformations.euler_from_quaternion(quaternion)
     yaw = euler[2]
-
-    #print(yaw)
-    
+   
+            
     err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
-    #heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.00001))
-    if not (err<0.5):
+    #heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.00001)
+    if not (err<0.5 or ((x_des-pos[0])<0.5) or ((y_des-pos[1])<0.5)):
         control_car(pos,yaw)
     else:
         print("Stopping car...")
         stop_car()
-    
-
-
-def set_throttle_steer(data):
-    #print(data.pose[1].orientation.y)
-    #racecar_pose = data.pose[1]
-    #print("callback", data.yaw)
-    #pos[0] = data.x
-    #pos[1] = data.y
-    #print(data)
-    #yaw = data.yaw
-    #print(yaw)
-    """
-
-    err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
-    #heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.00001))
-    if not (err<0.5):
-        control_car(pos,yaw)
-    """
-
+        sub.unregister()        
+        servo_commands()
+        
 
 def control_car(pos,yaw):
+    print("Navigating to",x_des, y_des)
     
     msg = AckermannDriveStamped()
     print("====position=====",pos[0],pos[1])
-    speed_control = PID_control.PID(0.000001,0,0.000001)
+    speed_control = PID_control.PID(1e-6,0,1e-6)
     err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
     throttle = speed_control.Update(err)
     print("distance:", err)
     print("throttle:",throttle)
-    #print("yaw:",yaw)
     
-    steer_control = PID_control.PID(0.000001,0,0.0000001)
+    
+    steer_control = PID_control.PID(1e-8,0,1e-7)
     heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.01))
     steer = steer_control.Update(heading-yaw)
     print("yaw:",yaw)
-    print("heading:",heading)
     print("steer_angle:",heading-yaw)
     print("steer:",steer)
     
@@ -96,73 +85,57 @@ def control_car(pos,yaw):
 def stop_car():
     msg = AckermannDriveStamped()
     msg.drive.speed = 0
+    msg.drive.steering_angle = 0
+    msg.drive.steering_angle_velocity = 0
     x_pub.publish(msg)
-    print("Goal Reached!")
-
+    print("Goal Reached!") 
 
 def servo_commands():
-
+    print("Enter Waypoints:")
     #time.sleep(2)
-    rospy.init_node('servo_commands', anonymous=True)
+    global x_des
+    global y_des
+    global sub
+    global count
+    count +=1
+    df.columns = df.columns.str.strip()
+
     
+        
+    x_des = float(input())
+    y_des = float(input())
+    #x_des = (df.X[8*count]/4)-0.125
+    #y_des = (df.Y[8*count]/4)-0.125
+    
+    #rospy.init_node('servo_commands', anonymous=True)   
     msg = AckermannDriveStamped()
     #rospy.Subscriber("/progress", Progress, set_throttle_steer)
-    rospy.Subscriber("/gazebo/model_states", ModelStates, set_position)
-    rospy.Subscriber("/progress", Progress, set_throttle_steer)
+    sub = rospy.Subscriber("/gazebo/model_states", ModelStates, set_position)   
 
-     
-    #err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
-    #heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.00001))
-    
-    #print("read yaw2",yaw)
     while not (rospy.is_shutdown()):
-        """    
+        """
         print("====position=====",pos[0],pos[1])
         speed_control = PID_control.PID(0.000001,0,0.000001)
         err = math.sqrt((x_des-pos[0])**2+(y_des-pos[1])**2)
         throttle = speed_control.Update(err)
         print("distance:", err)
         print("throttle:",throttle)
-        
-        steer_control = PID_control.PID(0.0001,0,0.000001)
-        heading = math.atan((y_des-pos[1])/(x_des-pos[0]+0.01))
-        steer = steer_control.Update(heading-yaw)
-        print("yaw:",yaw)
-        print("heading:",heading)
-        print("steer_angle:",heading-yaw)
-        print("steer:",steer)
-        
-       
-        #print("========throttle signal=======",throttle)
-        msg.drive.speed = throttle 
-        x_pub.publish(msg)
-        #time.sleep(1)
-               
-        msg.drive.steering_angle = steer
-        x_pub.publish(msg)
-        #time.sleep(1)
-        print("==============")
-        """
+        """       
 
-        
     msg.drive.speed = 0.0
     x_pub.publish(msg)
     time.sleep(1)
-
-    
-    """
-
-    while not rospy.is_shutdown:
-        msg.drive.speed = 1.0
-    	x_pub.publish(msg)
-	time.sleep(1)
-    """
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('servo_commands', anonymous=True)
+        
+        global df
+        df = pd.read_csv('route.csv',delim_whitespace=True)
+        
         servo_commands()
     except rospy.ROSInterruptException:
         pass
